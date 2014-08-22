4627bc05e0a1ca08ce4f5bd560f9ed5ece201f87
uscbsitric <usc_bsit_ric@yahoo.com>
2014-08-22 11:20:14 +0800

    ...

---
+++ phpClosuresTesting/phpClosureTest1.php
@@ -0,0 +1,10 @@
+<?php
+	$sampleVariable = function($sampleParam1, $sampleParam2)
+					  {
+					  		echo $sampleParam1;
+					  		echo "<br>";
+					  		echo $sampleParam2;
+					  };
+
+	$sampleVariable('frederick', 'sandalo');
+?>
