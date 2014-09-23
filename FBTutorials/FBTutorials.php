4417c8404e92e4b2eeff6f6ebf5bb96ebd17085b
uscbsitric <usc_bsit_ric@yahoo.com>
2014-09-23 18:47:31 +0800

    added FBTutorials

---
+++ FBTutorials/FBTutorials.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+<html>
+	<head>
+		<title>Facebook Integration</title>
+	</head>
+	<body>
+		<script>
+	      window.fbAsyncInit = function()
+						       {
+						       		FB.init({appId   : '529434107108789',
+									         xfbml   : true,
+									         version : 'v2.1'
+										     status  : true,
+										     cookie  : true
+						        			});
+						       };
+	
+	      (function(d, s, id)
+	       {
+	         var js, fjs = d.getElementsByTagName(s)[0];
+	         if (d.getElementById(id))
+		     {return;}
+
+	         js = d.createElement(s);
+	         js.id = id;
+	         js.src = "//connect.facebook.net/en_US/sdk.js";
+	         fjs.parentNode.insertBefore(js, fjs);
+	       }(document, 'script', 'facebook-jssdk'));
+	    </script>
+		
+		<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+
+
+	</body>
+</html>
